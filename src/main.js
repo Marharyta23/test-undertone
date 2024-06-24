cfaf948c06f2f29ea8d33ddaab9c4852def552f1
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-06-24 21:12:27 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -26,23 +26,38 @@ refs.mainText.addEventListener('animationend', () => {
 
 const swiper = {
   1: {
-    url: './img/carusel-1.png 1x, ./img/carusel-1-2x@2x.png 2x',
+    urls: {
+      desc: './img/carusel-1.png 1x, ./img/carusel-1-2x@2x.png 2x',
+      mob: './img/carusel-1-mob.png 1x, ./img/carusel-1-2x@2x-mob.png 2x',
+    },
     text: '“The first time I used the Samsung Bespoke Jet™, I cried. I’m not being sensational; I really did. Of course, this vacuum worked great. But that’s not all.”',
   },
   2: {
-    url: './img/carusel-2.png 1x, ./img/carusel-2-2x@2x.png 2x',
+    urls: {
+      desc: './img/carusel-2.png 1x, ./img/carusel-2-2x@2x.png 2x',
+      mob: './img/carusel-2-mob.png 1x, ./img/carusel-2-2x@2x-mob.png 2x',
+    },
     text: '“If you’re an over-cleaner, like myself, you’ll nerd out on all of the functions. If you avoid this chore at all costs, you’ll appreciate how simple Samsung makes it.”',
   },
   3: {
-    url: './img/carusel-3.png 1x, ./img/carusel-3-2x@2x.png 2x',
+    urls: {
+      desc: './img/carusel-3.png 1x, ./img/carusel-3-2x@2x.png 2x',
+      mob: './img/carusel-3-mob.png 1x, ./img/carusel-3-2x@2x-mob.png 2x',
+    },
     text: '“Both the floor and pet hair attachments are cleverly designed to eliminate the dreaded hair wrap. (In other words, you’ll never have to tackle hair tangles with a pair of scissors again.)”',
   },
   4: {
-    url: './img/carusel-4.png 1x, ./img/carusel-4-2x@2x.png 2x',
+    urls: {
+      desc: './img/carusel-4.png 1x, ./img/carusel-4-2x@2x.png 2x',
+      mob: './img/carusel-4-mob.png 1x, ./img/carusel-4-2x@2x-mob.png 2x',
+    },
     text: '“When I learned the Samsung Bespoke Vac cleaned itself with amazing technology, that’s when I cried. No more scraping spider legs and hair out of the crevices with my hands. Its suction power is so strong, the canister is left perfectly clean after every use. It’s like a vacuum for your vacuum.”',
   },
   5: {
-    url: './img/carusel-5.png 1x, ./img/carusel-5-2x@2x.png 2x',
+    urls: {
+      desc: './img/carusel-5.png 1x, ./img/carusel-5-2x@2x.png 2x',
+      mob: './img/carusel-5-mob.png 1x, ./img/carusel-5-2x@2x-mob.png 2x',
+    },
     text: '“Because it’s so nice-looking, it can live right in the kitchen. No more hauling a vacuum up and down the basement stairs on the daily”',
   },
 };
@@ -63,7 +78,20 @@ function autoplaySlides() {
 function slideTo(count) {
   refs.subText.textContent = swiper[`${count}`].text;
   refs.count.textContent = count;
-  refs.img.setAttribute('srcset', `${swiper[`${count}`].url}`);
+  refs.photoSection.innerHTML = `<picture>
+            <source
+              media="(max-width:767px)"
+              srcset=${swiper[`${count}`].urls.mob}
+            />
+            <source
+              media="(min-width:768px)"
+              srcset=${swiper[`${count}`].urls.desc}
+            />
+            <img
+              srcset=${swiper[`${count}`].urls.desc}
+              class="photo-carusel"
+            />
+          </picture>`;
 }
 
 autoplaySlides();
